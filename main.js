8f0accfab514a8fa0870aa0d8d7cfeb3dc21071d
sawyerbutton <lil103@pitt.edu>
2018-12-18 17:31:17 +0800

    added example 1

--- main.js
+++ main.js
@@ -11,8 +11,19 @@
  
 // ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
 
-var viewModel = {
-    details: ko.observable() // Initially blank
-};
-viewModel.details("<em>For further details, view the report <a href='report.html'>here</a>.</em>"); // HTML content appears
-ko.applyBindings(viewModel);
+// var viewModel = {
+//     details: ko.observable() // Initially blank
+// };
+// viewModel.details("<em>For further details, view the report <a href='report.html'>here</a>.</em>"); // HTML content appears
+// ko.applyBindings(viewModel);
+
+var viewModel = function(first, last) {
+    _self = this;
+    _self.firstName = ko.observable(first);
+    _self.lastName = ko.observable(last);
+    _self.fullName = ko.computed(function() {
+        return _self.firstName() +' ' + _self.lastName();
+    })
+}
+
+ko.applyBindings(new viewModel('a','b'));
